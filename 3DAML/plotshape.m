89940b98f11a470886a17a2be913417ac224c20e
Skallos-s <79016393+Skallos-s@users.noreply.github.com>
2022-05-03 14:03:43 -0400

    Add files via upload

---
+++ 3DAML/plotshape.m
@@ -0,0 +1,14 @@
+function plotshape(facedata, facelength, axes, color)
+
+r = 1;
+[x,~] = size(color);
+
+for j=1:length(facelength)
+    poly = facedata(:, r : r+facelength(j)-1);
+    r = r + facelength(j);
+    if [0,0,1]*normal(poly) > 0
+        fill(poly(1,:),poly(2,:),color(rem(j-1,x)+1,:))
+        axis(axes)
+        hold on
+    end
+end
